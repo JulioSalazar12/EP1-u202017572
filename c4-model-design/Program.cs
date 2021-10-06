d9b24628d4415507916378c3e257b65bc7e45af1
Julio12-prog <u202017572@upc.edu.pe>
2021-10-05 19:27:05 -0500

    feat: añadi Containers diagram

--- c4-model-design/Program.cs
+++ c4-model-design/Program.cs
@@ -22,22 +22,22 @@ namespace c4_model_design
             Model model = workspace.Model;
 
             // 1. Diagrama de Contexto
-            SoftwareSystem energySystem = model.AddSoftwareSystem("Lorem ipsum", "Lorem ipsum");
+            SoftwareSystem energySystem = model.AddSoftwareSystem("Sistema soporte de energia ecologica", "Sistema soporte de energia ecologica para Conductores de vehículos eléctricos, Operadores de punto de carga, Propietarios de flota y Proveedores de energía");
             SoftwareSystem googleMaps = model.AddSoftwareSystem("Google Maps", "Plataforma que ofrece una REST API para geolocalizacion referencial.");
             SoftwareSystem vehicleSystem = model.AddSoftwareSystem("Vehicle System", "Permite conocer la informacion de la carga del vehiculo");
             SoftwareSystem weatherSystem = model.AddSoftwareSystem("Weather System", "Permite conocer el clima para validar los costos o tarifas de carga");
 
-            Person conductor =  model.AddPerson("conductor", "clientte2");
-            Person operadorPC = model.AddPerson("operador", "cliendte2");
-            Person propietarioF = model.AddPerson("propietario flota", "cliente2");
-            Person proveedorE = model.AddPerson("proveedor energia", "cliesnte2");
+            Person conductor =  model.AddPerson("Conductor de cehiculo", "Conductor de cehiculo electrico");
+            Person operadorPC = model.AddPerson("CPO", "Operador de punto de carga");
+            Person propietarioF = model.AddPerson("Propietario de flota", "Empresario propietario de una flota de vehiculos electricos");
+            Person proveedorE = model.AddPerson("Proveedor de energia", "Empresa proveedora de energia");
             
-            conductor.Uses(energySystem, "Realiza consultas para mantenerse ");
-            operadorPC.Uses(energySystem, "Realiza consultas para mantenerse ");
-            propietarioF.Uses(energySystem, "Realiza consultas para mantenerse ");
-            proveedorE.Uses(energySystem, "Realiza consultas para mantenerse ");
-            energySystem.Uses(weatherSystem, "Lorem ipsum");
-            energySystem.Uses(vehicleSystem, "Lorem ipsum");
+            conductor.Uses(energySystem, "Realiza consultas para conocer ubicacion y reserva en CPO");
+            operadorPC.Uses(energySystem, "Realiza consultas para conocer la reserva de carga");
+            propietarioF.Uses(energySystem, "Realiza consultas para reservar turno de flota de vehiculos");
+            proveedorE.Uses(energySystem, "Realiza consultas para suminstrar energia correctamente");
+            energySystem.Uses(weatherSystem, "Usa el servicio de clima para estimar una tarifa correcta");
+            energySystem.Uses(vehicleSystem, "Usa el sistema del vehiculo para conocer la informacion de carga de cehiculo");
             energySystem.Uses(googleMaps, "Usa la API de google maps");
             
             SystemContextView contextView = viewSet.CreateSystemContextView(energySystem, "Contexto", "Diagrama de contexto");
@@ -62,66 +62,44 @@ namespace c4_model_design
             styles.Add(new ElementStyle("VehicleSystem") { Background = "#2f95c7", Color = "#ffffff", Shape = Shape.RoundedBox });
             styles.Add(new ElementStyle("WeatherSystem") { Background = "#FF3062", Color = "#ffffff", Shape = Shape.RoundedBox });
 
-            // // 2. Diagrama de Contenedores
-            // Container mobileApplication = energySystem.AddContainer("Mobile App", "Permite a los usuarios visualizar un dashboard con el resumen de toda la información del traslado de los lotes de vacunas.", "Flutter");
-            // Container webApplication = energySystem.AddContainer("Web App", "Permite a los usuarios visualizar un dashboard con el resumen de toda la información del traslado de los lotes de vacunas.", "Flutter Web");
-            // Container landingPage = energySystem.AddContainer("Landing Page", "", "Flutter Web");
-            // Container apiRest = energySystem.AddContainer("API Rest", "API Rest", "NodeJS (NestJS) port 8080");
-            // Container flightPlanningContext = energySystem.AddContainer("Flight Planning Context", "Bounded Context del Microservicio de Planificación de Vuelos", "NodeJS (NestJS)");
-            // Container airportContext = energySystem.AddContainer("Airport Context", "Bounded Context del Microservicio de información de Aeropuertos", "NodeJS (NestJS)");
-            // Container aircraftInventoryContext = energySystem.AddContainer("Aircraft Inventory Context", "Bounded Context del Microservicio de Inventario de Aviones", "NodeJS (NestJS)");
-            // Container vaccinesInventoryContext = energySystem.AddContainer("Vaccines Inventory Context", "Bounded Context del Microservicio de Inventario de Vacunas", "NodeJS (NestJS)");
-            // Container monitoringContext = energySystem.AddContainer("Monitoring Context", "Bounded Context del Microservicio de Monitoreo en tiempo real del status y ubicación del vuelo que transporta las vacunas", "NodeJS (NestJS)");
-            // Container database = energySystem.AddContainer("Database", "", "Oracle");
+            // 2. Diagrama de Contenedores
+            Container webApplication   = energySystem.AddContainer("Web App",                   "Permite a los usuarios visualizar la ubicacion, informacion y tarifa de la energia", "Flutter Web");
+            Container recargaContext   = energySystem.AddContainer("Recarga Context",           "Bounded Context del Microservicio de Recarga de vehiculo", "NodeJS (NestJS)");
+            Container ubicacionContext = energySystem.AddContainer("Geolocalizacion Context",   "Bounded Context del Microservicio de información de CPO", "NodeJS (NestJS)");
+            Container reservaContext   = energySystem.AddContainer("Reserva Inventory Context", "Bounded Context del Microservicio de horario de recargas", "NodeJS (NestJS)");            
+            Container database         = energySystem.AddContainer("Database", "", "Oracle");
             
-            // ciudadano.Uses(mobileApplication, "Consulta");
-            // ciudadano.Uses(webApplication, "Consulta");
-            // ciudadano.Uses(landingPage, "Consulta");
+            conductor.Uses(webApplication, "Consulta");
+            proveedorE.Uses(webApplication, "Consulta");
+            propietarioF.Uses(webApplication, "Consulta");
+            operadorPC.Uses(webApplication, "Consulta");
 
-            // mobileApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
-            // webApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
-
-            // apiRest.Uses(flightPlanningContext, "", "");
-            // apiRest.Uses(airportContext, "", "");
-            // apiRest.Uses(aircraftInventoryContext, "", "");
-            // apiRest.Uses(vaccinesInventoryContext, "", "");
-            // apiRest.Uses(monitoringContext, "", "");
+            webApplication.Uses(recargaContext, " x ", "JSON/HTTPS");
+            webApplication.Uses(ubicacionContext, " x ", "JSON/HTTPS");
+            webApplication.Uses(reservaContext, " x ", "JSON/HTTPS");
             
-            // flightPlanningContext.Uses(database, "", "JDBC");
-            // airportContext.Uses(database, "", "JDBC");
-            // aircraftInventoryContext.Uses(database, "", "JDBC");
-            // vaccinesInventoryContext.Uses(database, "", "JDBC");
-            // monitoringContext.Uses(database, "", "JDBC");
+            recargaContext.Uses(database, "", "JDBC");
+            ubicacionContext.Uses(database, "", "JDBC");
+            reservaContext.Uses(database, "", "JDBC");
             
-            // monitoringContext.Uses(googleMaps, "API Request", "JSON/HTTPS");
-            // monitoringContext.Uses(vehicleSystem, "API Request", "JSON/HTTPS");
+            ubicacionContext.Uses(googleMaps, "API Request", "JSON/HTTPS");
+            recargaContext.Uses(vehicleSystem, "API Request", "JSON/HTTPS");
+            recargaContext.Uses(weatherSystem, "API Request", "JSON/HTTPS");
 
-            // // Tags
-            // mobileApplication.AddTags("MobileApp");
-            // webApplication.AddTags("WebApp");
-            // landingPage.AddTags("LandingPage");
-            // apiRest.AddTags("APIRest");
-            // database.AddTags("Database");
-            // flightPlanningContext.AddTags("FlightPlanningContext");
-            // airportContext.AddTags("AirportContext");
-            // aircraftInventoryContext.AddTags("AircraftInventoryContext");
-            // vaccinesInventoryContext.AddTags("VaccinesInventoryContext");
-            // monitoringContext.AddTags("MonitoringContext");
+            // Tags
+            webApplication.AddTags("WebApp");
+            database.AddTags("Database");
+            recargaContext.AddTags("BoundedContext");
+            ubicacionContext.AddTags("BoundedContext");
+            reservaContext.AddTags("BoundedContext");
 
-            // styles.Add(new ElementStyle("MobileApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.MobileDevicePortrait, Icon = "" });
-            // styles.Add(new ElementStyle("WebApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
-            // styles.Add(new ElementStyle("LandingPage") { Background = "#929000", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
-            // styles.Add(new ElementStyle("APIRest") { Shape = Shape.RoundedBox, Background = "#0000ff", Color = "#ffffff", Icon = "" });
-            // styles.Add(new ElementStyle("Database") { Shape = Shape.Cylinder, Background = "#ff0000", Color = "#ffffff", Icon = "" });
-            // styles.Add(new ElementStyle("FlightPlanningContext") { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
-            // styles.Add(new ElementStyle("AirportContext") { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
-            // styles.Add(new ElementStyle("AircraftInventoryContext") { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
-            // styles.Add(new ElementStyle("VaccinesInventoryContext") { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
-            // styles.Add(new ElementStyle("MonitoringContext") { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
+            styles.Add(new ElementStyle("WebApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
+            styles.Add(new ElementStyle("Database") { Shape = Shape.Cylinder, Background = "#ff0000", Color = "#ffffff", Icon = "" });
+            styles.Add(new ElementStyle("BoundedContext") { Shape = Shape.Hexagon, Background = "#facc2e", Icon = "" });
 
-            // ContainerView containerView = viewSet.CreateContainerView(energySystem, "Contenedor", "Diagrama de contenedores");
-            // contextView.PaperSize = PaperSize.A4_Landscape;
-            // containerView.AddAllElements();            
+            ContainerView containerView = viewSet.CreateContainerView(energySystem, "Contenedor", "Diagrama de contenedores");
+            contextView.PaperSize = PaperSize.A4_Landscape;
+            containerView.AddAllElements();            
 
             structurizrClient.UnlockWorkspace(workspaceId);
             structurizrClient.PutWorkspace(workspaceId, workspace);
